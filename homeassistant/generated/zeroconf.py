037d223e134707f09df8199cca4a44a30a6da8cf
DrMatthiasBlaschke <blaschke@synertronixx.de>
2026-01-30 13:28:19 +0100

    Added zeroconf & zeroconf tests

--- homeassistant/generated/zeroconf.py
+++ homeassistant/generated/zeroconf.py
@@ -1006,6 +1006,11 @@ ZEROCONF = {
             "domain": "technove",
         },
     ],
+    "_tfa_me._tcp.local.": [
+        {
+            "domain": "tfa_me",
+        },
+    ],
     "_touch-able._tcp.local.": [
         {
             "domain": "apple_tv",
